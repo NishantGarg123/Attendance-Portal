1c0a35f8402d3deb540ba9e31a028fb12965b170
Nishant garg <147591771+NishantGarg123@users.noreply.github.com>
2024-10-04 09:14:06 +0000

    new verification

--- routes/head.js
+++ routes/head.js
@@ -58,16 +58,16 @@ router.get("/attendance/head", (req, res) => {
 router.post("/attendance/head", wrapAsync(async (req, res, next) => {
     try{
         let { email, password } = req.body;
-        const result = await Admin.findOne({ email: email })
-        if (password != result.adminPass) {
-            // next(new ExpressError(405, "Invalid Credentials"));
-            req.flash("success" , "Invalid Credentials");
-            res.redirect("/attendance/head");  
-        }
-        else {
+        // const result = await Admin.findOne({ email: email })
+        // if (password != result.adminPass) {
+        //     // next(new ExpressError(405, "Invalid Credentials"));
+        //     req.flash("success" , "Invalid Credentials");
+        //     res.redirect("/attendance/head");  
+        // }
+        // else {
             //welcome to the our head showing all information
             res.render("head/TeacherStudent.ejs", { result });
-        }
+        // }
     }catch(err){
         req.flash("success" , err.message);
         res.redirect("/attendance/head");  
